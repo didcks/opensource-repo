1ab33147e2583a5b7fad081c94b67d326d387106
Park Yechan <didcks8798@naver.com>
2023-12-08 00:43:53 +0900

    first add color

---
+++ 색 추가/Form2.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace 윈도우프로그래밍_프로젝트
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeTable = new System.Windows.Forms.DataGridView();
+            this.grade = new System.Windows.Forms.ComboBox();
+            this.firstGrade = new System.Windows.Forms.ComboBox();
+            this.fourthGrade = new System.Windows.Forms.ComboBox();
+            this.secondGrade = new System.Windows.Forms.ComboBox();
+            this.thirdGrade = new System.Windows.Forms.ComboBox();
+            this.color = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimeTable
+            // 
+            this.TimeTable.ColumnHeadersHeight = 46;
+            this.TimeTable.Location = new System.Drawing.Point(14, -1);
+            this.TimeTable.Name = "TimeTable";
+            this.TimeTable.RowHeadersWidth = 82;
+            this.TimeTable.RowTemplate.Height = 23;
+            this.TimeTable.Size = new System.Drawing.Size(674, 665);
+            this.TimeTable.TabIndex = 1;
+            this.TimeTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TimeTable_CellClick_1);
+            // 
+            // grade
+            // 
+            this.grade.FormattingEnabled = true;
+            this.grade.Items.AddRange(new object[] {
+            "first",
+            "second",
+            "third",
+            "fourth"});
+            this.grade.Location = new System.Drawing.Point(19, 670);
+            this.grade.Name = "grade";
+            this.grade.Size = new System.Drawing.Size(90, 20);
+            this.grade.TabIndex = 5;
+            this.grade.SelectedIndexChanged += new System.EventHandler(this.grade_SelectedIndexChanged);
+            // 
+            // firstGrade
+            // 
+            this.firstGrade.FormattingEnabled = true;
+            this.firstGrade.Items.AddRange(new object[] {
+            "C언어 기초",
+            "병신같은 과목",
+            "원어민 외국어"});
+            this.firstGrade.Location = new System.Drawing.Point(141, 670);
+            this.firstGrade.Name = "firstGrade";
+            this.firstGrade.Size = new System.Drawing.Size(95, 20);
+            this.firstGrade.TabIndex = 6;
+            this.firstGrade.Visible = false;
+            this.firstGrade.SelectedIndexChanged += new System.EventHandler(this.firstGrade_SelectedIndexChanged);
+            // 
+            // fourthGrade
+            // 
+            this.fourthGrade.FormattingEnabled = true;
+            this.fourthGrade.Items.AddRange(new object[] {
+            "취업",
+            "해야지",
+            "이런"});
+            this.fourthGrade.Location = new System.Drawing.Point(141, 670);
+            this.fourthGrade.Name = "fourthGrade";
+            this.fourthGrade.Size = new System.Drawing.Size(95, 20);
+            this.fourthGrade.TabIndex = 7;
+            this.fourthGrade.Visible = false;
+            // 
+            // secondGrade
+            // 
+            this.secondGrade.FormattingEnabled = true;
+            this.secondGrade.Items.AddRange(new object[] {
+            "2학년과목",
+            "고급프로그래밍"});
+            this.secondGrade.Location = new System.Drawing.Point(141, 670);
+            this.secondGrade.Name = "secondGrade";
+            this.secondGrade.Size = new System.Drawing.Size(95, 20);
+            this.secondGrade.TabIndex = 8;
+            this.secondGrade.Visible = false;
+            this.secondGrade.SelectedIndexChanged += new System.EventHandler(this.secondGrade_SelectedIndexChanged);
+            // 
+            // thirdGrade
+            // 
+            this.thirdGrade.FormattingEnabled = true;
+            this.thirdGrade.Items.AddRange(new object[] {
+            "3학년과목1",
+            "빵상",
+            "데이터베이스"});
+            this.thirdGrade.Location = new System.Drawing.Point(141, 670);
+            this.thirdGrade.Name = "thirdGrade";
+            this.thirdGrade.Size = new System.Drawing.Size(95, 20);
+            this.thirdGrade.TabIndex = 9;
+            this.thirdGrade.Visible = false;
+            // 
+            // color
+            // 
+            this.color.FormattingEnabled = true;
+            this.color.Items.AddRange(new object[] {
+            "lightBlue",
+            "pink",
+            "yellow"});
+            this.color.Location = new System.Drawing.Point(515, 697);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(100, 20);
+            this.color.TabIndex = 11;
+            this.color.SelectedIndexChanged += new System.EventHandler(this.color_SelectedIndexChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 751);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.thirdGrade);
+            this.Controls.Add(this.secondGrade);
+            this.Controls.Add(this.fourthGrade);
+            this.Controls.Add(this.firstGrade);
+            this.Controls.Add(this.grade);
+            this.Controls.Add(this.TimeTable);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TimeTable;
+        private System.Windows.Forms.ComboBox grade;
+        private System.Windows.Forms.ComboBox firstGrade;
+        private System.Windows.Forms.ComboBox fourthGrade;
+        private System.Windows.Forms.ComboBox secondGrade;
+        private System.Windows.Forms.ComboBox thirdGrade;
+        private System.Windows.Forms.ComboBox color;
+    }
+}
