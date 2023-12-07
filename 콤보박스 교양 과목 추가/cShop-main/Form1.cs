ad059ce2b7f9d5eabe28753d643e009d82bada30
Park Yechan <didcks8798@naver.com>
2023-12-08 02:08:24 +0900

    combobox culture subject add

---
+++ 콤보박스 교양 과목 추가/cShop-main/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 윈도우프로그래밍_프로젝트
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "아이디를 입력해 주세요.") txtUsername.Text = "";
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "") txtUsername.Text = "아이디를 입력해 주세요.";
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "비밀번호를 입력해 주세요.")
+            {
+                txtPassword.Text = "";
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "비밀번호를 입력해 주세요.";
+                txtPassword.PasswordChar = '\0';
+            }
+        }
+
+        private void Button1_click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "123" && password == "123")
+            {
+                ShowTimetableForm();
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패. 아이디와 비밀번호를 확인하세요.", "로그인 실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ShowTimetableForm()
+        {
+            Form2 timetableForm = new Form2();
+
+            timetableForm.Show();
+
+            this.Hide();
+        }
+
+    }
+}
