cad210cd7f0a03143c60ade4bb64d104db4d23b2
Park Yechan <didcks8798@naver.com>
2023-12-07 23:21:36 +0900

    first timetable

---
+++ 게시판 처음/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeTable = new System.Windows.Forms.DataGridView();
+            this.PlusButton = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimeTable
+            // 
+            this.TimeTable.Location = new System.Drawing.Point(12, 12);
+            this.TimeTable.Name = "TimeTable";
+            this.TimeTable.RowTemplate.Height = 23;
+            this.TimeTable.Size = new System.Drawing.Size(650, 654);
+            this.TimeTable.TabIndex = 0;
+            // 
+            // PlusButton
+            // 
+            this.PlusButton.Location = new System.Drawing.Point(375, 693);
+            this.PlusButton.Name = "PlusButton";
+            this.PlusButton.Size = new System.Drawing.Size(124, 47);
+            this.PlusButton.TabIndex = 1;
+            this.PlusButton.Text = "추가";
+            this.PlusButton.UseVisualStyleBackColor = true;
+            this.PlusButton.Click += new System.EventHandler(this.PlusButton_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(125, 693);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(116, 21);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.Text = "입력";
+            this.TextBox.Click += new System.EventHandler(this.TextBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 846);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.PlusButton);
+            this.Controls.Add(this.TimeTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TimeTable;
+        private System.Windows.Forms.Button PlusButton;
+        private System.Windows.Forms.TextBox TextBox;
+    }
+}
+
---
+++ 게시판 처음/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        DataGridViewCell selectedCell; //datagridviewcell <- 이게 표임
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimetable(); //시작할 때 같이 실행
+
+        }
+
+        private void InitializeTimetable()
+        {
+            TimeTable.RowTemplate.Height = 20;
+
+            // 초기설정 datagridview <- 이게 표임
+            TimeTable.ColumnCount = 6; // 교시 열(0번 열은 교시 사용)
+            TimeTable.Rows.Add("교시", "월요일", "화요일", "수요일", "목요일", "금요일");
+            for (int i = 0; i < 6; i++)
+            {
+                TimeTable.RowTemplate.Height = 70;
+            }
+
+            // 요일 설정
+            TimeTable.Rows.Add("1교시");
+            TimeTable.Rows.Add("2교시");
+            TimeTable.Rows.Add("3교시");
+            TimeTable.Rows.Add("4교시");
+            TimeTable.Rows.Add("5교시");
+            TimeTable.Rows.Add("6교시");
+            TimeTable.Rows.Add("7교시");
+            TimeTable.Rows.Add("8교시");
+
+
+            // 요일과 교시 셀들 눌러서 못 바꾸게 하는 거임(2차원 배열)
+            for (int i = 0; i < TimeTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < TimeTable.Columns.Count; j++)
+                {
+                    DataGridViewCell cell = TimeTable.Rows[i].Cells[j];
+                    cell.ReadOnly = true; // 읽을 수만 있게
+                }
+            }
+
+            // DataGridView 셀 클릭 이벤트 추가
+            TimeTable.CellClick += TimeTable_CellClick; //cellclick 이벤트에 함수 추가
+
+            // 변경 버튼 클릭 이벤트 추가
+            PlusButton.Click += PlusButton_Click; //마찬가지 
+        }
+
+        private void TimeTable_CellClick(object sender, DataGridViewCellEventArgs e) // 셀 눌렀을 떄
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0) //행렬이 0보다 큰 걸 눌렀을 떄(제대로 된 거 눌렀을 떄 만 실행한다는 거임)
+            {
+                selectedCell = TimeTable.Rows[e.RowIndex].Cells[e.ColumnIndex]; //선택된 셀을 이걸로 바꿈
+            }
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e) //추가 버튼 클릭했을떄
+        {
+            if (selectedCell != null) //셀 선택 됐을떄만 실행
+            {
+                string newContent = TextBox.Text.Trim(); // 변경할 새로운 내용(trim이 텍스트박스 가져오는거)
+
+                // newcontent << textbox 내용
+                selectedCell.Value = newContent; //이거 값 newContent로 채움
+            }
+            else
+            {
+                MessageBox.Show("셀을 먼저 선택하세요."); //안됐으면 선택하라 뜸
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Click(object sender, EventArgs e)
+        {
+            TextBox.Text = string.Empty; //텍스트박스에 입력 누르면 바로 지워지게
+        }
+    }
+}
