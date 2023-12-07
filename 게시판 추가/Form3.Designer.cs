c1a44502426f6b85c341541e8a50bb41f05fb050
Park Yechan <didcks8798@naver.com>
2023-12-08 00:29:06 +0900

    add notice board

---
+++ 게시판 추가/Form3.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace 윈도우프로그래밍_프로젝트
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.btnCreatePost = new System.Windows.Forms.Button();
+            this.lstPosts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(12, 30);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 21);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.Text = "제목";
+            this.txtTitle.Enter += new System.EventHandler(this.txtTitle_Enter);
+            this.txtTitle.Leave += new System.EventHandler(this.txtTitle_Leave);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 57);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(300, 100);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.Text = "내용";
+            this.txtContent.Enter += new System.EventHandler(this.txtContent_Enter);
+            this.txtContent.Leave += new System.EventHandler(this.txtContent_Leave);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(12, 187);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(150, 21);
+            this.txtAuthor.TabIndex = 2;
+            this.txtAuthor.Text = "작성자";
+            this.txtAuthor.Enter += new System.EventHandler(this.txtAuthor_Enter);
+            this.txtAuthor.Leave += new System.EventHandler(this.txtAuthor_Leave);
+            // 
+            // btnCreatePost
+            // 
+            this.btnCreatePost.Location = new System.Drawing.Point(212, 393);
+            this.btnCreatePost.Name = "btnCreatePost";
+            this.btnCreatePost.Size = new System.Drawing.Size(100, 30);
+            this.btnCreatePost.TabIndex = 3;
+            this.btnCreatePost.Text = "저장";
+            this.btnCreatePost.UseVisualStyleBackColor = true;
+            this.btnCreatePost.Click += new System.EventHandler(this.btnCreatePost_Click);
+            // 
+            // lstPosts
+            // 
+            this.lstPosts.FormattingEnabled = true;
+            this.lstPosts.ItemHeight = 12;
+            this.lstPosts.Location = new System.Drawing.Point(12, 214);
+            this.lstPosts.Name = "lstPosts";
+            this.lstPosts.Size = new System.Drawing.Size(300, 148);
+            this.lstPosts.TabIndex = 5;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 450);
+            this.Controls.Add(this.lstPosts);
+            this.Controls.Add(this.btnCreatePost);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Button btnCreatePost;
+        private System.Windows.Forms.ListBox lstPosts;
+    }
+}
---
+++ 게시판 추가/Form3.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 윈도우프로그래밍_프로젝트
+{
+    public partial class Form3 : Form
+    {
+        private List<Post> posts = new List<Post>();
+
+        public Form3()
+        {
+            InitializeComponent();
+
+            txtTitle.Text = "";
+
+            txtTitle.Enter += txtTitle_Enter;
+
+            txtTitle.Leave += txtTitle_Leave;
+
+            txtContent.Text = "";
+
+            txtContent.Enter += txtContent_Enter;
+
+            txtContent.Leave += txtContent_Leave;
+
+            txtAuthor.Text = "";
+
+            txtAuthor.Enter += txtAuthor_Enter;
+
+            txtAuthor.Leave += txtAuthor_Leave;
+        }
+
+        private void ClearInputFields()
+        {
+            txtTitle.Text = string.Empty;
+            txtContent.Text = string.Empty;
+            txtAuthor.Text = string.Empty;
+        }
+    
+
+    private void btnCreatePost_Click(object sender, EventArgs e)
+        {
+            // 사용자로부터 입력 받은 데이터로 게시물 객체 생성
+            Post post = new Post
+            {
+                Title = txtTitle.Text,
+                Content = txtContent.Text,
+                Author = txtAuthor.Text
+            };
+
+            // 게시물을 리스트에 추가
+            posts.Add(post);
+
+            // 리스트 박스에 게시물 추가
+            lstPosts.Items.Add(post.Title);
+
+            // 입력 필드 초기화
+            ClearInputFields();
+        }
+
+        private void txtTitle_Enter(object sender, EventArgs e)
+        {
+            if (txtTitle.Text == "제목을 입력해 주세요.")
+            {
+                txtTitle.Text = "";
+            }
+        }
+
+        private void txtTitle_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                txtTitle.Text = "제목을 입력해 주세요.";
+            }
+        }
+
+      
+
+        private void txtContent_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtContent.Text))
+            {
+                txtContent.Text = "내용을 입력해 주세요.";
+            }
+        }
+
+        private void txtContent_Enter(object sender, EventArgs e)
+        {
+            if (txtContent.Text == "내용을 입력해 주세요.")
+            {
+                txtContent.Text = "";
+            }
+        }
+
+        private void txtAuthor_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAuthor.Text))
+            {
+                txtAuthor.Text = "작성자를 입력해 주세요.";
+            }
+        }
+
+        private void txtAuthor_Enter(object sender, EventArgs e)
+        {
+            if (txtAuthor.Text == "작성자를 입력해 주세요.")
+            {
+                txtAuthor.Text = "";
+            }
+        }
+    }
+}
+
+
+
